fcfcc92ff66f7587db5a9a8cef70fc94a6900ce0
techmentors2024 <techmentors2024@gmail.com>
2025-01-20 19:09:09 +0700

    update UI

---
+++ src/main/java/co/istad/streamsavro/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package co.istad.streamsavro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configureSecurity(HttpSecurity http) throws Exception {
+
+        // Config endpoints
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                .requestMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+        );
+
+        // Config form login
+        http.formLogin(form -> form
+                .loginPage("/login")
+                .usernameParameter("user_username")
+                .passwordParameter("user_password")
+        );
+
+        return http.build();
+    }
+
+}
