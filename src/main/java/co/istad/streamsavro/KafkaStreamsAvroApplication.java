9e44f699b8e8c9b690398a1a0febb839e0390163
techmentors2024 <techmentors2024@gmail.com>
2025-01-24 17:37:14 +0700

    test
    # Conflicts:
    #	src/main/java/co/istad/streamsavro/config/BatchConfig.java
    #	src/main/java/co/istad/streamsavro/config/WebSocketConfig.java
    #	src/main/java/co/istad/streamsavro/consumer/DownloadJobConsumer.java
    #	src/main/java/co/istad/streamsavro/domain/DownloadJobRepository.java
    #	src/main/resources/application.yml
    #	src/main/resources/static/statistic.js

--- src/main/java/co/istad/streamsavro/KafkaStreamsAvroApplication.java
+++ src/main/java/co/istad/streamsavro/KafkaStreamsAvroApplication.java
@@ -1,6 +1,7 @@
 package co.istad.streamsavro;
 
 import co.istad.streamsavro.avro.ElectronicOrder;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
